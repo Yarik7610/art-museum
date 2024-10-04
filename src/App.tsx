c304b22281f5e9d131eb858eadeb7efea7c2a57a
Yaroslav Yanovich <arikarik782@gmail.com>
2024-10-04 12:25:22 +0300

    Mark up footer

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
 import "./App.css"
+import { Footer } from "./components/Footer"
 import { Header } from "./components/Header"
 
 function App() {
   return (
-    <div className="App">
+    <>
       <Header />
-    </div>
+      <main></main>
+      <Footer />
+    </>
   )
 }
 
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,19 +1,20 @@
 import bookmark from "../assets/bookmark.svg"
 import home from "../assets/home.svg"
 import logo from "../assets/logo.svg"
+
 export const Header = () => {
   return (
     <header>
       <div className="header__content">
-        <img src={logo} />
+        <img src={logo} alt="Museum of art" />
         <nav className="header__nav">
           <ul>
             <li>
-              <img src={home} />
+              <img src={home} alt="Home icon" />
               <span>Home</span>
             </li>
             <li>
-              <img src={bookmark} />
+              <img src={bookmark} alt="Bookmark icon" />
               <span>Your favourites</span>
             </li>
           </ul>
