3f958d2a83b697be746ac2a0345ae7fec2727a9b
Yaroslav Yanovich <arikarik782@gmail.com>
2024-10-04 19:25:11 +0300

    Create OtherPaintings component

---
+++ src/components/OtherPaintings.tsx
@@ -0,0 +1,15 @@
+import { Painting } from "../pages/Home"
+import { PaintingShortInfo } from "./PaintingShortInfo"
+
+interface OtherPaintingsProps {
+  otherPaintings: Painting[]
+}
+export const OtherPaintings: React.FC<OtherPaintingsProps> = ({ otherPaintings }) => {
+  return (
+    <section className="other-paintings">
+      {otherPaintings.map((p) => (
+        <PaintingShortInfo withImg={true} />
+      ))}
+    </section>
+  )
+}
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,4 +1,5 @@
 import { useState } from "react"
+import { OtherPaintings } from "../components/OtherPaintings"
 import { PaintingCardSection } from "../components/PaintingCardsSection"
 import { Search } from "../components/Search"
 import { SectionHeading } from "../components/SectionHeading"
@@ -9,6 +10,7 @@ export interface Painting {
 export const Home = () => {
   const [query, setQuery] = useState("")
   const [paintings, setPaintings] = useState<Painting[]>([])
+  const [otherPaintings, setOtherPaintings] = useState<Painting[]>([])
   const [currentPage, setCurrentPage] = useState(1)
 
   return (
@@ -17,14 +19,31 @@ export const Home = () => {
         Let's find some <span className="">Art</span> here!
       </h1>
       <Search query={query} setQuery={setQuery} />
-      <SectionHeading text1="Topics for you" text2="Our special gallery" />
-      <PaintingCardSection
-        currentPage={currentPage}
-        setCurrentPage={setCurrentPage}
-        totalPaintingsCount={5}
-        paintings={[{ type: "Public" }, { type: "Public" }, { type: "Public" }]}
-      />
-      <SectionHeading text1="Here some more" text2="Other works for you" />
+      <section className="home-section">
+        <SectionHeading text1="Topics for you" text2="Our special gallery" />
+        <PaintingCardSection
+          currentPage={currentPage}
+          setCurrentPage={setCurrentPage}
+          totalPaintingsCount={5}
+          paintings={[{ type: "Public" }, { type: "Public" }, { type: "Public" }]}
+        />
+      </section>
+      <section className="home-section">
+        <SectionHeading text1="Here some more" text2="Other works for you" />
+        <OtherPaintings
+          otherPaintings={[
+            { type: "Public" },
+            { type: "Public" },
+            { type: "Public" },
+            { type: "Public" },
+            { type: "Public" },
+            { type: "Public" },
+            { type: "Public" },
+            { type: "Public" },
+            { type: "Public" }
+          ]}
+        />
+      </section>
     </>
   )
 }
