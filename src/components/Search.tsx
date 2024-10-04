74b66de4727fa9c2115b77d037079b028aa55f53
Yaroslav Yanovich <arikarik782@gmail.com>
2024-10-04 17:10:21 +0300

    Create Search component

---
+++ src/components/Search.tsx
@@ -0,0 +1,24 @@
+import search from "../assets/search.svg"
+
+interface SearchProps {
+  query: string
+  setQuery: React.Dispatch<React.SetStateAction<string>>
+}
+export const Search: React.FC<SearchProps> = ({ query, setQuery }) => {
+  const handleOnChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setQuery(e.target.value)
+  }
+
+  return (
+    <div className="search">
+      <input
+        value={query}
+        onChange={handleOnChange}
+        className="search__input"
+        type="text"
+        placeholder="Search Art, Artist, Work..."
+      />
+      <img src={search} alt="Search icon" />
+    </div>
+  )
+}
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,9 +1,17 @@
+import { useState } from "react"
+import { Search } from "../components/Search"
+import { SectionHeading } from "../components/SectionHeading"
+
 export const Home = () => {
+  const [query, setQuery] = useState("")
+
   return (
     <>
       <h1 className="big-heading">
         Let's find some <span className="">Art</span> here!
       </h1>
+      <Search query={query} setQuery={setQuery} />
+      <SectionHeading text1="Topics for you" text2="Our special gallery" />
     </>
   )
 }
