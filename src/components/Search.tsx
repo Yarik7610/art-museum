327b0644adf09181318e722c85d45f368b27889f
Yaroslav Yanovich <arikarik782@gmail.com>
2024-10-06 16:59:20 +0300

    Add react-hook-form for search input

--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -1,34 +1,44 @@
 import search from "#assets/search.svg"
-import { useCallback, useState } from "react"
+import { useCallback, useEffect } from "react"
+import { SubmitHandler, useForm } from "react-hook-form"
 import { debounce } from "../utils/debounce"
 
 interface SearchProps {
   setQuery: React.Dispatch<React.SetStateAction<string>>
 }
 
+interface FormState {
+  value: string
+}
 export const Search = ({ setQuery }: SearchProps) => {
-  const [value, setValue] = useState("")
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors }
+  } = useForm<FormState>()
 
   const debouncedSetQuery = useCallback(
     debounce((query: string) => setQuery(query), 400),
     []
   )
 
-  const handleOnChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setValue(e.target.value)
-    debouncedSetQuery(e.target.value)
+  useEffect(() => {
+    const subscription = watch((state) => onSubmit(state as FormState))
+    return () => subscription.unsubscribe()
+  }, [watch])
+
+  const onSubmit: SubmitHandler<FormState> = (data) => {
+    debouncedSetQuery(data.value)
   }
 
   return (
     <div className="search">
-      <input
-        value={value}
-        onChange={handleOnChange}
-        className="search__input"
-        type="text"
-        placeholder="Search Art, Artist, Work..."
-      />
-      <img src={search} alt="Search icon" />
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <input {...register("value")} className="search__input" type="text" placeholder="Search Art, Artist, Work..." />
+        {errors?.value && <p>{errors.value.message}</p>}
+        <img src={search} alt="Search icon" />
+      </form>
     </div>
   )
 }
